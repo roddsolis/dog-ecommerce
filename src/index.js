dc0ef0943d4590bb7e89e2eadf6cd8e337dc83f0
Rodrigo Solis <rodrigosolis@F1D00461K399.local>
2021-10-12 22:48:17 -0300

    primera entrega react js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react"
 // 2- tiene que estar la variable reactDOM en scope
 import ReactDOM from "react-dom"
 // 3- tengo que tener un elemento
-const Elemento = () => <p>Hola Mundo</p>
+const Elemento = () => <p>otra frase de prueba</p>
 // 4- tengo que renderizar el elemento
 ReactDOM.render(<Elemento/> , document.getElementById('root'))
 
